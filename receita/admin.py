4077f6acaf19d3c2d585e68bb9359b2f5b3b7b0a
Antonio Hannesch Junior <hanneschjr@gmail.com>
2026-04-08 15:31:46 -0300

    Implementando template minhas_receitas.html

--- receita/admin.py
+++ receita/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Receita
 
 # Register your models here.
+
+class Receitas(admin.ModelAdmin):
+    list_display = ['titulo', 'categoria', ]
+    search_fields = ['titulo',]
+    list_filter = ['categoria', 'data_criacao']
+
+
+
+admin.site.register(Categoria)
+# admin.site.register(Receita)
+admin.site.register(Receita, Receitas)
+
--- receita/views.py
+++ receita/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Receita, Categoria
 
 # Create your views here.
+def receitas(request):
+    receitas = Receita.objects.all()
+    context = {'receitas': receitas}
+    
+    return render(request, 'minhas_receitas.html', context)
